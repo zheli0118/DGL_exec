680ddf83d8daa0387d4c641dbf9bf6d22afa35ea
admin <zhelishenyh@gmain.com>
2023-09-07 19:29:21 +0800

    updata GraphSAGE

--- ownGNN.py
+++ ownGNN.py
@@ -7,27 +7,92 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+
 class SAGEConv(nn.Module):
     def __init__(self, in_feat, out_feat):
         super().__init__()
         self.out_feat = out_feat
         self.in_feat = in_feat
-        self.linear = nn.Linear(2*in_feat,out_feat)
-    def forward(self,g,h):
+        self.linear = nn.Linear(2 * in_feat, out_feat)
+
+    def forward(self, g, h):
         """
         :param g: input graph
         :param h: feature
         """
         with g.local_scope():
             g.ndata["h"] = h
+            # 注意这里是g.update,所以fn的各个函数将src的特征复制到目标特征
             g.update_all(
-                message_func=fn.copy_u("h","m"),
-                reduce_func=fn.mean("m","h_N"),
+                message_func=fn.copy_u("h", "m"),
+                reduce_func=fn.mean("m", "h_N"),
             )
-            h_N=g.ndata["h_N"]
-            h_total= torch.cat([h,h_N],dim=1)
+            h_N = g.ndata["h_N"]
+            h_total = torch.cat([h, h_N], dim=1)
             return self.linear(h_total)
-if __name__ == '__main__':
-    (g1,g2),_=dgl.load_graphs("constructV1.dgl")
-    with g2.local_scope():
-        print("1")
+
+
+class Model(nn.Module):
+    def __init__(self, in_feats, h_feats, num_classes):
+        super().__init__()
+        self.conv1 = SAGEConv(in_feats, h_feats)
+        self.conv2 = SAGEConv(h_feats, num_classes)
+
+    def forward(self, g, feature):
+        h = self.conv1(g, feature)
+        h = nn.ReLU(h)
+        output = self.conv2(g, h)
+        return output
+
+import dgl.data
+dataset = dgl.data.CoraGraphDataset("E:\py_project\DGL_exec\data\cora\cora_v2_d697a464")
+g = dataset[0]
+def train(g, model):
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+    all_logits = []
+    best_val_acc = 0
+    best_test_acc = 0
+
+    features = g.ndata["feat"]
+    labels = g.ndata["label"]
+    train_mask = g.ndata["train_mask"]
+    val_mask = g.ndata["val_mask"]
+    test_mask = g.ndata["test_mask"]
+    for e in range(200):
+        # Forward
+        logits = model(g, features)
+
+        # Compute prediction
+        pred = logits.argmax(1)
+
+        # Compute loss
+        # Note that we should only compute the losses of the nodes in the training set,
+        # i.e. with train_mask 1.
+        loss = F.cross_entropy(logits[train_mask], labels[train_mask])
+
+        # Compute accuracy on training/validation/test
+        train_acc = (pred[train_mask] == labels[train_mask]).float().mean()
+        val_acc = (pred[val_mask] == labels[val_mask]).float().mean()
+        test_acc = (pred[test_mask] == labels[test_mask]).float().mean()
+
+        # Save the best validation accuracy and the corresponding test accuracy.
+        if best_val_acc < val_acc:
+            best_val_acc = val_acc
+            best_test_acc = test_acc
+
+        # Backward
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        all_logits.append(logits.detach())
+
+        if e % 5 == 0:
+            print(
+                "In epoch {}, loss: {:.3f}, val acc: {:.3f} (best {:.3f}), test acc: {:.3f} (best {:.3f})".format(
+                    e, loss, val_acc, best_val_acc, test_acc, best_test_acc
+                )
+            )
+
+
+model = Model(g.ndata["feat"].shape[1], 16, dataset.num_classes)
+train(g, model)
